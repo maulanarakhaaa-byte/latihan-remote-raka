9db1bba8dd67f633a8f1f2cfb75a293e89be9830
zakk369 <zakknasrul@gmail.com>
2026-07-31 20:56:26 +0700

    Perubahan terjadi pada index.php, yang awalnya <title>NihonGo! - Belajar Bahasa Jepang</title> menjadi <title>NihonGo! - Belajar Bahasa Nippon</title>

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ if (isLoggedIn()) redirect('user/dashboard.php');
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>NihonGo! - Belajar Bahasa Jepang</title>
+<title>NihonGo! - Belajar Bahasa Nippon</title>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800;900&family=Noto+Serif+JP:wght@400;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="assets/css/base.css">
